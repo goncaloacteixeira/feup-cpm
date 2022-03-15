be06764257449a2ef3bef4eca0c10b0fbef8b722
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2022-03-15 17:45:43 +0000

    first commit

---
+++ FirstApp/app/src/main/java/org/feup/cpm/skdgt/firstapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.feup.cpm.skdgt.firstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "org.feup.cpm.skdgt.firstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.bt_send).setOnClickListener { _ ->
+            val message = findViewById<EditText>(R.id.edt_message).text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra(EXTRA_MESSAGE, message)
+            startActivity(intent)
+        }
+    }
+}
